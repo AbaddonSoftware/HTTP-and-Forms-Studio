77d778173f05b2d7b46f06089863a6ed5ac1a86b
Donnie Baker <donnie.l.baker@gmail.com>
2020-05-11 23:06:57 -0400

    separated the error code from the validation code

--- search.js
+++ search.js
@@ -1,21 +1,26 @@
 function init() {
     let form = document.getElementById("searchForm");
-    const searchOptions = {
+    let errorBox = document.getElementById("error");
+    let searchTextExists;
+    let checkedRadioExists;
+    let searchOptions = {
         'google': 'https://google.com/search',
         'bing': 'https://bing.com/search',
         'duckduckgo': 'https://duckduckgo.com/',
         'ask': 'https://ask.com/web'
     };
 
-    function validateInput() {
-        let errorBox = document.getElementById("error");
+    function printError() {
         errorBox.innerHTML = "";
-        let searchTextExists = document.querySelector("input[name=q]").value !== "";
-        let checkedRadioExists = document.querySelector("input[name=engine]:checked");
         let invalidSearchText = "<span> Error: Please input text for us to search for. </span>";
         let invalidSelectRadio = "<span> Error: Please select a search engine for us to search. </span>";
         searchTextExists ? null : errorBox.innerHTML += invalidSearchText;
         checkedRadioExists ? null : errorBox.innerHTML += invalidSelectRadio;
+    }
+
+    function validateInput() {
+        searchTextExists = document.querySelector("input[name=q]").value;
+        checkedRadioExists = document.querySelector("input[name=engine]:checked");
         return searchTextExists && checkedRadioExists;
     }
 
@@ -25,6 +30,7 @@ function init() {
             form.setAttribute("action", formAction);
         }
         else {
+            printError();
             event.preventDefault();
         }
     });
